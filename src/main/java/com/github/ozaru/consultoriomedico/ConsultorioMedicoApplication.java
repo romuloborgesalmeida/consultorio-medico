3a8d74522136524fb956bc7fd63806f9fe629123
Diego Davel <diegodavel@gmail.com>
2020-01-23 10:57:45 -0300

    init

---
+++ src/main/java/com/github/ozaru/consultoriomedico/ConsultorioMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.github.ozaru.consultoriomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ozaru/consultoriomedico/ConsultorioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ozaru.consultoriomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
